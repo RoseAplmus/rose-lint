7974130d4a4ad491ddf726b1f67d5a685f1b0cad
Alicehhhmm <1245500390@qq.com>
2024-12-09 17:55:28 +0800

    foo: text commit-ms

--- scripts/verify-commit.js
+++ scripts/verify-commit.js
@@ -1,21 +1,25 @@
 // @ts-check
-import pico from 'picocolors'
-import { readFileSync } from 'node:fs'
-import path from 'node:path'
+import pico from "picocolors";
+import { readFileSync } from "node:fs";
+import path from "node:path";
 
-const msgPath = path.resolve('.git/COMMIT_EDITMSG')
-const msg = readFileSync(msgPath, 'utf-8').trim()
+const msgPath = path.resolve(".git/COMMIT_EDITMSG");
+const msg = readFileSync(msgPath, "utf-8").trim();
 
-const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+const commitRE =
+  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/;
+console.log("commit Test:", commitRE);
 
 if (!commitRE.test(msg)) {
-    console.log()
-    console.error(
-        `  ${pico.white(pico.bgRed(' ERROR '))} ${pico.red(`invalid commit message format.`)}\n\n` +
-            pico.red(`  Proper commit message format is required for automated changelog generation. Examples:\n\n`) +
-            `    ${pico.green(`feat(model): add 'eslint' option`)}\n` +
-            `    ${pico.green(`fix(lint): unpdate lint config (#21)`)}\n\n` +
-            pico.red(`  See .github/commit-convention.md for more details.\n`)
-    )
-    process.exit(1)
+  console.log();
+  console.error(
+    `  ${pico.white(pico.bgRed(" ERROR "))} ${pico.red(`invalid commit message format.`)}\n\n` +
+      pico.red(
+        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`,
+      ) +
+      `    ${pico.green(`feat(model): add 'eslint' option`)}\n` +
+      `    ${pico.green(`fix(lint): unpdate lint config (#21)`)}\n\n` +
+      pico.red(`  See .github/commit-convention.md for more details.\n`),
+  );
+  process.exit(1);
 }
